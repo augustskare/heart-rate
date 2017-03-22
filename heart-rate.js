0d3bb2b255f8f3d294beec1ddcab1f0af98cce74
August Skare <post@augustskare.no>
2017-03-22 19:34:58 +0100

    loading state

--- heart-rate.js
+++ heart-rate.js
@@ -30,13 +30,27 @@ css.innerHTML = `
   }
 
   .icon {
-    fill: #bd3e3e;
+    color: #bd3e3e; 
     width: auto;
     height: 2em;
     margin-right: .5em;
 
     animation: heartbeat 1s infinite alternate both ease-in-out;
     transform-origin: center;
+  }
+
+  .loading .icon {
+    animation: loading 10s infinite forwards linear;
+    fill: none;
+    stroke-dasharray: 70px;
+    stroke-dashoffset: 500px;
+  }
+
+
+  @keyframes loading {
+    to {
+      stroke-dashoffset: 0;
+    }
   }
 
   @keyframes heartbeat {
@@ -46,28 +60,22 @@ css.innerHTML = `
   }
 `;
 
-const hearRate = document.createElement('div');
-hearRate.innerHTML = `
-  <svg class="icon" width="20" height="19" viewBox="0 0 20 19" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
-    <path d="M10 18.35l-1.45-1.32C3.4 12.36 0 9.28 0 5.5 0 2.42 2.42 0 5.5 0 7.24 0 8.91.81 10 2.09 11.09.81 12.76 0 14.5 0 17.58 0 20 2.42 20 5.5c0 3.78-3.4 6.86-8.55 11.54L10 18.35z"/>
-  </svg>
-  <span class="heart-rate">0</span> 
-`;
-
 class HeartRate extends HTMLElement {
   constructor() {
     super();
 
     this.monitor = new HeartRateMonitor();
+    this.gotInitalValue = false;
 
     this.connectButton = document.createElement('button');
     this.connectButton.textContent = 'Connect heart rate monitor';
 
     this.root = this.attachShadow({ mode: 'open' });
-    
     this.root.appendChild(css);
     this.root.appendChild(this.connectButton);
     
+    this._createTemplate();
+
     this._onConnect = this._onConnect.bind(this);
     this._onChange = this._onChange.bind(this);
   }
@@ -80,22 +88,38 @@ class HeartRate extends HTMLElement {
     this.connectButton.removeEventListener('click', this._onConnect);
   }
 
+  _createTemplate() {
+    this.template = document.createElement('div');
+    this.template.classList.add('loading');
+    this.template.innerHTML = `
+      <svg class="icon" width="22" height="20" viewBox="0 0 22 20" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" fill="currentColor" fill-rule="nonzero">
+        <title>Heart</title>
+        <path d="M15.5 1c-1.74 0-3.41.81-4.5 2.09C9.91 1.81 8.24 1 6.5 1 3.42 1 1 3.42 1 6.5c0 3.78 3.4 6.86 8.55 11.54L11 19.35l1.45-1.32C17.6 13.36 21 10.28 21 6.5 21 3.42 18.58 1 15.5 1z"/>
+      </svg>
+      <span class="heart-rate">0</span> 
+    `;
+  }
+
   _onConnect(event) {
     this.root.removeChild(this.connectButton);
-    this.root.appendChild(hearRate);
+    this.root.appendChild(this.template);
 
     this.monitor.connect().then(device => {
-      this.heartRate = hearRate.querySelector('.heart-rate');
+      this.heartRate = this.template.querySelector('.heart-rate');
       console.log(`Connected to: ${device.name}`);
       this.monitor.sensorLocation().then(value => console.log(`Device location: ${value}`));
       this.monitor.onValueChange(this._onChange);
     }).catch(error => {
-      this.root.removeChild(hearRate);
+      this.root.removeChild(this.template);
       this.root.appendChild(this.connectButton);
     });
   }
 
   _onChange(value) {
+    if (!this.gotInitalValue) {
+      this.template.classList.remove('loading');
+      this.gotInitalValue = true;
+    }
     this.heartRate.textContent = value;
   }
 
